1a4c680fe4c49eb4099a0fdd4877a78ee77247fa
Miguel Monllau <59914220+Devnuvols@users.noreply.github.com>
2024-06-09 18:27:22 +0200

    Initial Commit

---
+++ mktest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mktest
+//
+//  Created by Miguel Monllau on 9/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mktest/mktestApp.swift
@@ -0,0 +1,17 @@
+//
+//  mktestApp.swift
+//  mktest
+//
+//  Created by Miguel Monllau on 9/6/24.
+//
+
+import SwiftUI
+
+@main
+struct mktestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
